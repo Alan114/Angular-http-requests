b3dbbd4fb0f2e89936fdbd0c3ff6ffb01300b61e
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-13 14:23:02 +0200

    Setting headers in a http request

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
@@ -29,7 +29,10 @@ export class PostsService {
   fetchPosts() {
     return this.http
       .get<{ [key: string]: Post }>(
-        "https://angular-http-requests-4dc8c-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
+        "https://angular-http-requests-4dc8c-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+        {
+          headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+        }
       )
       .pipe(
         map((responseData) => {
