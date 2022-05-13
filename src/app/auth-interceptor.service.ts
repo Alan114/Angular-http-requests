c56a41c588af21307e89016d1b9d90a2af129fa5
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-13 15:33:29 +0200

    Making an interceptor service

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,17 @@
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log("Request is on its way");
+    return next.handle(req);
+  }
+  constructor() {}
+}
